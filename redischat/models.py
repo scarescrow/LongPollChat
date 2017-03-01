9fc381120262232b54f5b7a990256197ffa129c2
sagnikroy <sagnik.sinha@edupristine.ca>
2017-03-02 03:03:02 +0530

    Refactor models to models.py

--- redischat/models.py
+++ redischat/models.py
@@ -1,5 +1,60 @@
 from __future__ import unicode_literals
 
 from django.db import models
+import redis, json
+from threading import Thread
 
 # Create your models here.
+class Message:
+    """
+        Class to hold message details for each
+        incoming message
+    """
+    def __init__(self, payload):
+        self.sender = payload['sender']
+        self.receiver = payload['receiver']
+        self.date = payload['time']
+        self.body = payload['message']
+
+class Listener(Thread):
+    """
+        Threaded class to start client in a separate
+        thread which will be dedicated to listen to
+        Redis queue, and process each incoming message
+    """
+    def __init__(self, channel):
+        Thread.__init__(self)
+        self.redis = redis.Redis()
+        self.ps = self.redis.pubsub()
+        self.ps.subscribe(channel)
+        self.messages = {}
+
+    def storeMessages(self, payload):
+        # Store messages in a dict till next 
+        # API call is fired
+        payload = json.loads(payload)
+        message = Message(payload)
+        receiver = message.receiver
+        if receiver not in self.messages:
+            self.messages[receiver] = []
+        self.messages[receiver].append(message)
+
+    def getPendingMessages(self, receiver):
+        # Retrieve all messages that have reached 
+        # since last call. Then clear dict, so 
+        # that overhead is not big
+        if receiver not in self.messages:
+            return []
+        messages = self.messages[receiver]
+        self.messages[receiver] = []
+        return messages
+
+    def run(self):
+        # Infinite listener for Redis queue
+        for item in self.ps.listen():
+            print item
+            if item['data'] == 'KILL':
+                self.ps.unsubscribe()
+                break
+            if item['data'] != 1:
+                self.storeMessages(item['data'])
--- redischat/views.py
+++ redischat/views.py
@@ -2,66 +2,13 @@ from django.shortcuts import render
 from django.http import HttpResponse
 import redis, json
 from datetime import datetime as dt
-from threading import Thread
+
+from .models import Message, Listener
 
 CHANNEL_NAME = 'messages'
 LISTENER = None
 
 # Create your views here.
-
-class Message:
-    """
-        Class to hold message details for each
-        incoming message
-    """
-    def __init__(self, payload):
-        self.sender = payload['sender']
-        self.receiver = payload['receiver']
-        self.date = payload['time']
-        self.body = payload['message']
-
-class Listener(Thread):
-    """
-        Threaded class to start client in a separate
-        thread which will be dedicated to listen to
-        Redis queue, and process each incoming message
-    """
-    def __init__(self, channel):
-        Thread.__init__(self)
-        self.redis = redis.Redis()
-        self.ps = self.redis.pubsub()
-        self.ps.subscribe(channel)
-        self.messages = {}
-
-    def storeMessages(self, payload):
-        # Store messages in a dict till next 
-        # API call is fired
-        payload = json.loads(payload)
-        message = Message(payload)
-        receiver = message.receiver
-        if receiver not in self.messages:
-            self.messages[receiver] = []
-        self.messages[receiver].append(message)
-
-    def getPendingMessages(self, receiver):
-        # Retrieve all messages that have reached 
-        # since last call. Then clear dict, so 
-        # that overhead is not big
-        if receiver not in self.messages:
-            return []
-        messages = self.messages[receiver]
-        self.messages[receiver] = []
-        return messages
-
-    def run(self):
-        # Infinite listener for Redis queue
-        for item in self.ps.listen():
-            print item
-            if item['data'] == 'KILL':
-                self.ps.unsubscribe()
-                break
-            if item['data'] != 1:
-                self.storeMessages(item['data'])
 
 def hello(request):
     # Test API endpoint to check if server is running
